321e2052ec6302aa8f33d7eeb36780b64ec7cf04
Carlos Estevez <54195505+Cestevez21@users.noreply.github.com>
2024-06-24 23:45:12 -0600

    add port env

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function verifyToken(req, res, next) {
       next();
     });
   }
-
-app.listen(3000, "0.0.0.0", () => {
+let port = process.env.PORT ?? 9090
+app.listen(port, "0.0.0.0", () => {
   console.log('Server running on port 3000');
 });
